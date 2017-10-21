66f67aa3fa1a30f1bc3e3954eafc76d66b6e4d1f
Alexander Rodriguez <alexrod2121@gmail.com>
2017-10-20 22:33:09 -0500

    Conlusion del UnitTest con Moq y HTML5

--- Cibertec/CodeCoverage.bat
+++ Cibertec/CodeCoverage.bat
@@ -3,5 +3,6 @@ SET currentPath=%~dp0
 SET testPath=%currentPath%Cibertec.WebApi.Test
 SET targetargs="test -f netcoreapp2.0 -c Release \"%testPath%\Cibertec.WebApi.Test.csproj\""
 
-opencover.console.exe -target:"dotnet.exe" -targetargs:%targetargs% -mergeoutput -hideskipped:File -output:coverage.xml -oldStyle -filter:"+[Cibertec.*]* -[Cibertec.RepositoriesTest*]* -[Cibertec.WebApi.Test*]*" -searchdirs:"%testPath%\bin\Release\netcoreapp2.0" -register:user
+opencover.console.exe -target:"dotnet.exe" -targetargs:%targetargs% -mergeoutput -hideskipped:File -output:coverage.xml -oldStyle -filter:"+[Cibertec.*]* -[Cibertec.RepositoriesTests*]* -[Cibertec.WebApi.Test*]*" -searchdirs:"%testPath%\bin\Release\netcoreapp2.0" -register:user
+
 reportgenerator.exe -reports:coverage.xml -targetdir:coverage -verbosity:Error
